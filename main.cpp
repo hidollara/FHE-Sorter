fa9d80ad25d3c44f0f69c1bcc7f5f32d70b96b6d
hidollara <hidollara@users.noreply.github.com>
2018-05-14 15:35:39 +0900

    refactor main.cpp

--- main.cpp
+++ main.cpp
@@ -2,15 +2,24 @@
 #include "bitonic.hpp"
 #include <iostream>
 
-int main() {
-    Comparator<int> *c = new IntComparator;
-    Sorter<int> *s = new BitonicSorter<int>(c);
-
+void run(Sorter<int> s) {
     std::vector<int> v{1, 6, 4, 7, 3, 2, 5, 8};
-    s->sort(v.begin(), v.end());
 
     for (auto x : v) std::cout << x << ' ';
     std::cout << std::endl;
 
+    s->sort(v.begin(), v.end());
+
+    for (auto x : v) std::cout << x << ' ';
+    std::cout << std::endl;
+}
+
+int main() {
+    Comparator<int> *c = new IntComparator;
+
+    Sorter<int> *bs = new BitonicSorter<int>(c);
+    run(bs);
+
     return 0;
 }
+
