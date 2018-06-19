10ad35afdcc63de78d91eb41def1fcab87dcd30c
hidollara <hidollara@users.noreply.github.com>
2018-06-19 16:45:25 +0900

    sort 8 elements

--- FHESort.cpp
+++ FHESort.cpp
@@ -8,6 +8,10 @@ Encrypter e;
 
 void run(Sorter<Ctxt> *s) {
     std::vector<Ctxt> v{
+        e.encrypt(0),
+        e.encrypt(1),
+        e.encrypt(1),
+        e.encrypt(0),
         e.encrypt(1),
         e.encrypt(0),
         e.encrypt(1),
@@ -27,9 +31,11 @@ int main() {
     Comparator<Ctxt> *c = new FHEComparator(e);
     Sorter<Ctxt> *s;
 
+    std::cout << "bitonic sort" << std::endl;
     s = new BitonicSorter<Ctxt>(c);
     run(s);
 
+    std::cout << "oddeven sort" << std::endl;
     s = new OddEvenSorter<Ctxt>(c);
     run(s);
 
